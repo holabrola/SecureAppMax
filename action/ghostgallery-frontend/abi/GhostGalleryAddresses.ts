81d3e6ab822b36844877bf48cc7bee34ac8389fa
holabrola <holabrola@proton.me>
2025-10-27 19:58:56 +0100

    Add GhostGalleryAddresses.ts

---
+++ action/ghostgallery-frontend/abi/GhostGalleryAddresses.ts
@@ -0,0 +1,12 @@
+export const GhostGalleryAddresses = {
+  "31337": {
+    "address": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+    "chainId": 31337,
+    "chainName": "Hardhat"
+  },
+  "11155111": {
+    "address": "0x332e85E874F409E497cA0f7e2D47610Cdba8F618",
+    "chainId": 11155111,
+    "chainName": "Sepolia"
+  }
+} as const;
